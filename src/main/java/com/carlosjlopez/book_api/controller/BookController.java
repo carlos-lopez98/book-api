cba9ed2af6c747d3182ff48fc5d3e5066001a296
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-08 20:19:44 -0700

    Working on retrieving books by ID

--- src/main/java/com/carlosjlopez/book_api/controller/BookController.java
+++ src/main/java/com/carlosjlopez/book_api/controller/BookController.java
@@ -4,6 +4,7 @@ import com.carlosjlopez.book_api.model.Book;
 import com.carlosjlopez.book_api.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,10 @@ public class BookController {
     @GetMapping
     public List<Book> getAllBooks(){
         return bookService.getAllBooks();
+    }
+
+    @GetMapping("/books/{id}")
+    public Book getBookById(@PathVariable("id") long id){
+        return bookService.getBookById(id);
     }
 }
--- src/main/java/com/carlosjlopez/book_api/service/BookService.java
+++ src/main/java/com/carlosjlopez/book_api/service/BookService.java
@@ -1,6 +1,8 @@
 package com.carlosjlopez.book_api.service;
 
 import com.carlosjlopez.book_api.model.Book;
+import com.carlosjlopez.book_api.repository.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,9 +16,27 @@ import java.util.List;
 @Service
 public class BookService {
 
+    @Autowired
+    private BookRepo bookRepo;
+
 
     //Returns a dummy book for now
     public List<Book> getAllBooks(){
         return List.of(new Book("Microservices", "Sam Newman"));
+    }
+
+
+    /**
+     * Function is used to return books by id
+     *
+     * Relies on access to the in-memory repo storage
+     * @param id
+     * @return Book
+     */
+    public Book getBookById(long id) {
+        Book res = bookRepo.books.stream()
+                .filter((x) -> x.getId() == id)
+
+        return res;
     }
 }
